443e9634945d296208249e163e944ffa94a59ab8
seanleavy <seanleavy@gmail.com>
2017-01-10 04:58:26 +0000

    Added part of search history

--- app/src/main/java/com/wii/sean/wiimmfiitus/activities/MkWiiHomeActivity.java
+++ app/src/main/java/com/wii/sean/wiimmfiitus/activities/MkWiiHomeActivity.java
@@ -1,11 +1,13 @@
 package com.wii.sean.wiimmfiitus.activities;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -14,7 +16,9 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -24,31 +28,38 @@ import android.widget.Toast;
 
 import com.wii.sean.wiimmfiitus.R;
 import com.wii.sean.wiimmfiitus.adapters.CustomWiiCyclerViewAdapter;
-import com.wii.sean.wiimmfiitus.friendSearch.Constants.FriendCodes;
 import com.wii.sean.wiimmfiitus.friendSearch.MkFriendSearch;
 import com.wii.sean.wiimmfiitus.model.MiiCharacter;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MkWiiHomeActivity extends AppCompatActivity {
 
     private RecyclerView wiiCyclerView;
+    private SwipeRefreshLayout swipeRefreshCardsLayout;
     private CustomWiiCyclerViewAdapter wiiAdapter;
     private RecyclerView.LayoutManager recyclerLayoutManager;
     private FloatingActionButton startButton;
     private ImageView wiimfiiIcon;
-    private ImageView nintendoLogo;
     private TextView miisFoundTextViewLabel;
     private TextView miisFoundTextViewValue;
     private ItemTouchHelper miiItemTouchHelper;
     private ItemTouchHelper.SimpleCallback simpleMiiItemTouchCallback;
     private ProgressBar progressBar;
     private AlertDialog.Builder alertDialogBuilder;
+    private Snackbar firstRunSnackbar;
+    private EditText friendCodeEditText;
 
+    private SharedPreferences savedSearches;
+    private SharedPreferences.Editor searchEditor;
     private MkFriendSearch mkFriendSearch;
     private int friendsFound = 0;
     private String searchTag = null;
+    private String searchPreferencesKey = "searchesMade";
+    private Set<String> searchHistoryResultSet;
 
     private List<MiiCharacter> wiiList = new ArrayList<>();
 
@@ -58,6 +69,17 @@ public class MkWiiHomeActivity extends AppCompatActivity {
         mkFriendSearch = new MkFriendSearch();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mk_wii_home_activity);
+        View parentCoordinatorLayout = findViewById(R.id.home_main_layout);
+        swipeRefreshCardsLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_to_refresh_cards);
+        firstRunSnackbar = Snackbar.make(parentCoordinatorLayout, R.string.first_run_message, Snackbar.LENGTH_LONG);
+        firstRunSnackbar.setActionTextColor(getResources().getColor(android.R.color.holo_red_dark));
+        firstRunSnackbar.show();
+
+        // To save every fCode search
+        final LayoutInflater layoutInflater = getLayoutInflater();
+        savedSearches = this.getSharedPreferences(getApplication().getPackageName(), MODE_PRIVATE);
+        searchEditor = savedSearches.edit();
+        searchHistoryResultSet = savedSearches.getStringSet(searchPreferencesKey, new HashSet<String>());
 
         startButton = (FloatingActionButton) findViewById(R.id.button_search_frame);
         wiiCyclerView = (RecyclerView) findViewById(R.id.home_screen_recycler_view);
@@ -65,7 +87,6 @@ public class MkWiiHomeActivity extends AppCompatActivity {
         miisFoundTextViewLabel = (TextView) findViewById(R.id.miis_found_label);
         miisFoundTextViewValue = (TextView) findViewById(R.id.miis_found_value);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar_search);
-//        nintendoLogo = (ImageView) findViewById(R.id.nintendo_logo_imageview);
 
         wiiCyclerView.setHasFixedSize(false);
         recyclerLayoutManager = new LinearLayoutManager(this);
@@ -74,14 +95,17 @@ public class MkWiiHomeActivity extends AppCompatActivity {
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                nintendoLogo.setVisibility(View.INVISIBLE);
+                View searchDialogView = layoutInflater.inflate(R.layout.friend_code_dialog, null);
+                // building the Search Dialog
                 alertDialogBuilder = new AlertDialog.Builder(view.getContext());
                 AlertDialog searchDialog;
-                final LayoutInflater layoutInflater = getLayoutInflater();
-                final View dialogView = layoutInflater.inflate(R.layout.friend_code_dialog, null);
-                alertDialogBuilder.setView(dialogView);
-                final EditText friendCodeEditText = (EditText) dialogView.findViewById(R.id.friend_code_edittext);
-                final Button deleteEdittext = (Button) dialogView.findViewById(R.id.delete_friend_code);
+                alertDialogBuilder.setView(searchDialogView);
+
+                final TextView history = (TextView) searchDialogView.findViewById(R.id.search_history);
+                friendCodeEditText = (EditText) searchDialogView.findViewById(R.id.friend_code_edittext);
+                final Button deleteEdittext = (Button) searchDialogView.findViewById(R.id.delete_friend_code);
+
+                //Empty the friend code editext field
                 deleteEdittext.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -106,37 +130,53 @@ public class MkWiiHomeActivity extends AppCompatActivity {
                         }
                     }
                 });
+
                 alertDialogBuilder.setTitle(R.string.dialog_title)
-                        .setMessage(R.string.dialog_body).setPositiveButton("Search", new DialogInterface.OnClickListener() {
+                        .setMessage(R.string.dialog_body)
+
+                        // SEARCH Button
+                        .setPositiveButton("Search", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        String friendCode = friendCodeEditText.getText().toString();
-                        new FriendSearchAsyncTask().execute(friendCode);
-                        startButton.setClickable(false);
-                        startButton.setVisibility(View.INVISIBLE);
-                        progressBar.setVisibility(View.VISIBLE);
+                        submitSearch(friendCodeEditText);
+                        addToSearchHistory(friendCodeEditText.getText().toString());
+                        dialog.dismiss();
                     }
-                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                })
+                        // CANCEL Button
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-//                        nintendoLogo.setVisibility(View.VISIBLE);
                     }
-                }).setNeutralButton("BumChums", new DialogInterface.OnClickListener() {
+                })
+                       // Default search
+                        .setNeutralButton("BumChums", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-//                                nintendoLogo.setVisibility(View.INVISIBLE);
                                 new FriendSearchAsyncTask().execute("");
                                 startButton.setClickable(false);
                                 startButton.setVisibility(View.INVISIBLE);
                                 progressBar.setVisibility(View.VISIBLE);
                             }
                         });
+
+                // Clicking the HISTORY textlabel
+                history.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+//                        showSearchHistoryDialog();
+                    }
+                });
+
+                // auto show keyboard
                 searchDialog = alertDialogBuilder.create();
                 searchDialog.getWindow().setSoftInputMode(LayoutParams.SOFT_INPUT_STATE_VISIBLE);
                 searchDialog.show();
 //                needs to be on 2 lines for some reason
                 final Button searchButton = searchDialog.getButton(AlertDialog.BUTTON_POSITIVE);
                 searchButton.setEnabled(false);
+
+                //disable search button if textfield isnt full
                 friendCodeEditText.addTextChangedListener(new TextWatcher() {
                     @Override
                     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -159,6 +199,7 @@ public class MkWiiHomeActivity extends AppCompatActivity {
             }
         });
 
+        //Listener on icon at top of view
         wiimfiiIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -180,6 +221,13 @@ public class MkWiiHomeActivity extends AppCompatActivity {
                 wiiAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
             }
         };
+
+        swipeRefreshCardsLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                submitSearch(friendCodeEditText);
+            }
+        });
         simpleMiiItemTouchCallback.getSwipeVelocityThreshold(0f);
         miiItemTouchHelper = new ItemTouchHelper(simpleMiiItemTouchCallback);
         miiItemTouchHelper.attachToRecyclerView(wiiCyclerView);
@@ -208,5 +256,55 @@ public class MkWiiHomeActivity extends AppCompatActivity {
             startButton.setClickable(true);
             progressBar.setVisibility(View.GONE);
         }
+
+    }
+
+    private void submitSearch(EditText friendCodeEditText) {
+        String friendCode = friendCodeEditText.getText().toString();
+        new FriendSearchAsyncTask().execute(friendCode);
+        startButton.setClickable(false);
+        startButton.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void addToSearchHistory(String search) {
+        if(!searchHistoryResultSet.contains(search)) {
+            searchHistoryResultSet.add(search);
+            saveSearchHistory();
+        }
+    }
+
+    private void saveSearchHistory() {
+        searchEditor.putStringSet(searchPreferencesKey, searchHistoryResultSet);
+        searchEditor.commit();
+    }
+
+
+    //todo use spinner
+    private void showSearchHistoryDialog() {
+        View searchHistoryDialogView = getLayoutInflater().inflate(R.layout.search_history_dialog, null);
+        final ArrayAdapter<String> searchResultsAdapter = new ArrayAdapter<>(searchHistoryDialogView.getContext(),
+                R.layout.search_history_row,
+                searchHistoryResultSet.toArray(new String[searchHistoryResultSet.size()]));
+
+        AlertDialog.Builder searchDialogBuilder = new AlertDialog.Builder(searchHistoryDialogView.getContext());
+        searchDialogBuilder.setSingleChoiceItems(searchResultsAdapter, 0, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                friendCodeEditText.setText(searchResultsAdapter.getItem(which));
+                dialog.dismiss();
+            }
+        });
+        searchDialogBuilder.setView(searchHistoryDialogView);
+        AlertDialog searchHistoryDialog = searchDialogBuilder.create();
+        searchHistoryDialog.getWindow().setLayout(600, 400);
+        searchHistoryDialog.getWindow().clearFlags(LayoutParams.FLAG_DIM_BEHIND);
+        searchDialogBuilder.show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        saveSearchHistory();
     }
 }
