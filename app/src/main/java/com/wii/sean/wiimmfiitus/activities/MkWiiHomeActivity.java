3c527fb52da5eacff6f6dd8ffb3a16056eb2489a
seanleavy <seanleavy@gmail.com>
2017-01-15 08:16:24 +0000

    removed unused interface TAKE 2

--- app/src/main/java/com/wii/sean/wiimmfiitus/activities/MkWiiHomeActivity.java
+++ app/src/main/java/com/wii/sean/wiimmfiitus/activities/MkWiiHomeActivity.java
@@ -2,33 +2,20 @@ package com.wii.sean.wiimmfiitus.activities;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.LayoutRes;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Adapter;
 
 import com.wii.sean.wiimmfiitus.R;
 import com.wii.sean.wiimmfiitus.adapters.CustomFragmentStatePagerAdapter;
-import com.wii.sean.wiimmfiitus.adapters.CustomWiiCyclerViewAdapter;
 import com.wii.sean.wiimmfiitus.fragments.BaseFragment;
-import com.wii.sean.wiimmfiitus.fragments.FavouritesFragment;
-import com.wii.sean.wiimmfiitus.fragments.MiiSearchFragment;
-import com.wii.sean.wiimmfiitus.helpers.PreferencesManager;
-import com.wii.sean.wiimmfiitus.model.MiiCharacter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-public class MkWiiHomeActivity extends AppCompatActivity implements BaseFragment.OnFragmentInteractionListener, BaseFragment.UpdateInterface {
+public class MkWiiHomeActivity extends AppCompatActivity implements BaseFragment.OnFragmentInteractionListener {
 
     private ViewPager mViewPager;
     private CustomFragmentStatePagerAdapter customFragmentPagerAdapter;
-    private List<MiiCharacter> miiFavourites;
     private List<PreferenceUpdateListener> listeners;
 
     public interface PreferenceUpdateListener {
@@ -49,10 +36,6 @@ public class MkWiiHomeActivity extends AppCompatActivity implements BaseFragment
     @Override
     public void onFragmentInteraction(Uri uri) {
 
-    }
-
-    @Override
-    public void updateFavouritesView() {
     }
 
     public synchronized void registerPreferenceUPdateListener(PreferenceUpdateListener listener) {
